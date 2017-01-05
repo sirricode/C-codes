fcd09e9fb987f3a074f25e3f436c1742b2585d90
sirricode <sirrikerimhakan@gmail.com>
2017-01-05 23:06:48 +0300

    Update SelectionSort.c

--- SelectionSort.c
+++ SelectionSort.c
@@ -2,24 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
+#define NELEMS(x)  (sizeof(x) / sizeof((x)[0])) // dizi eleman sayısını bulan makro
 
 
 int dizidoldur(int *ptr,int);
-int dizibas(int *ptr,int);
+int dizibas(int *ptr,int);		//prototipleme
 int dizisirala(int *ptr,int);
+
+
 int i,j;
+
+
 int main(int argc, char *argv[]) {
 	srand( time( NULL ) );
 	
 	int dizi[100];
 	
 	
-	dizidoldur(	dizi,	NELEMS(dizi));
+	dizidoldur(	dizi,	NELEMS(dizi));		//dizi dolduran fonksiyon
 	
-	dizisirala(	dizi,	NELEMS(dizi));
+	dizisirala(	dizi,	NELEMS(dizi));		//dizinin değerlerini sıralayan fonksiyon
 	
-	dizibas(	dizi,	NELEMS(dizi));
+	dizibas(	dizi,	NELEMS(dizi));		//diziyi ekrana bastıran fonksiyon
 	
 	
 	return 0;
